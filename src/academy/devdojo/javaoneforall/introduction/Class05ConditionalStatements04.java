5f0f0391beeb69cf708c59e6e49d506fccba5285
Silvio M Souza <silviomartinssouza1@gmail.com>
2026-03-14 20:40:00 -0300

    Upgrade Class05ConditionalStatements04

--- src/academy/devdojo/javaoneforall/introduction/Class05ConditionalStatements04.java
+++ src/academy/devdojo/javaoneforall/introduction/Class05ConditionalStatements04.java
@@ -3,9 +3,9 @@ package academy.devdojo.javaoneforall.introduction;
 
 public class Class05ConditionalStatements04 {
     static void main(String[] args) {
-        double annualSalary = 43350D;
+        double annualSalary = 70000D;
         double tax =    annualSalary <= 34712 ? annualSalary * 0.097 :
-                        annualSalary >= 34713 && annualSalary <= 68507 ? annualSalary * 0.3735 :
+                        annualSalary <= 68507 ? annualSalary * 0.3735 :
                         annualSalary * 0.495;
         System.out.println(tax);
     }
