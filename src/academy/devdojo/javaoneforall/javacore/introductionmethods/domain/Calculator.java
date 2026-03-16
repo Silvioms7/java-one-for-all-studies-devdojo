9d57bbb5c03d231ca2f85300d9c48dd5ef43c39d
Silvio M Souza <silviomartinssouza1@gmail.com>
2026-03-16 08:34:40 -0300

    Add dir introductionmethods, dir domain - Calculator and dir test - CalculatorTest01

---
+++ src/academy/devdojo/javaoneforall/javacore/introductionmethods/domain/Calculator.java
@@ -0,0 +1,39 @@
+package academy.devdojo.javaoneforall.javacore.introductionmethods.domain;
+
+public class Calculator {
+    public void sumTwoNumber() {
+        System.out.println(85343+7812);
+    }
+
+    public void subtractTwoNumbers() {
+        System.out.println(235-213);
+    }
+
+    public void multiplyTwoNumbers(int num1, int num2) {
+        System.out.println(num1 * num2);
+    }
+
+    public double divideTwoNumbers(double num1, double num2) {
+        if (num2 == 0) {
+            return 0;
+        }
+        return num1/num2;
+    }
+
+    public void printTwoNumbers2(double num1, double num2) {
+        if (num2 == 0) {
+            System.out.println("Can't divide by 0");
+            return;
+        }
+            System.out.println(num1 / num2);
+    }
+
+    public void changeTwoNumber(int num1, int num2) {
+        num1 = 34;
+        num2 = 65;
+        System.out.println("Inside changeTwoNumber");
+        System.out.println("num1: " + num1);
+        System.out.println("num2: " + num2);
+    }
+}
+
