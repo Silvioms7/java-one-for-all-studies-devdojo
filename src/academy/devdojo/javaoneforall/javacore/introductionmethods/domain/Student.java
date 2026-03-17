04199ae628c769dbdf61ba61294e5e86ac35d337
Silvio M Souza <silviomartinssouza1@gmail.com>
2026-03-17 16:47:19 -0300

    Add classes Student, StudentPrinter and StudentTest01

---
+++ src/academy/devdojo/javaoneforall/javacore/introductionmethods/domain/Student.java
@@ -0,0 +1,7 @@
+package academy.devdojo.javaoneforall.javacore.introductionmethods.domain;
+
+public class Student {
+    public String name;
+    public int age;
+    public char gender;
+}
---
+++ src/academy/devdojo/javaoneforall/javacore/introductionmethods/test/StudentTest01.java
@@ -0,0 +1,22 @@
+package academy.devdojo.javaoneforall.javacore.introductionmethods.test;
+
+import academy.devdojo.javaoneforall.javacore.introductionmethods.domain.Student;
+import academy.devdojo.javaoneforall.javacore.introductionmethods.domain.StudentPrinter;
+
+public class StudentTest01 {
+    static void main(String[] args) {
+        Student student01 = new Student();
+        Student student02 = new Student();
+        StudentPrinter printer = new StudentPrinter();
+        student01.name = "Barreal";
+        student01.age = 18;
+        student01.gender = 'M';
+
+        student02.name = "GabiGol";
+        student02.age = 19;
+        student02.gender = 'M';
+
+        printer.print(student01);
+        printer.print(student02);
+    }
+}
